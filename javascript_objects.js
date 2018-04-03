00b0335c72e7cd577d34555639b40807132cb6bc
devbaggett <devabaggett@gmail.com>
2018-04-02 18:17:12 -0700

    first commit

---
+++ javascript_objects.js
@@ -0,0 +1,54 @@
+// Assignment: JavaScript Objects
+
+// Complete the following challenges.
+
+
+// Challenge 1
+// Print all of the students and their cohort.
+
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+// for (var i = 0; i < students.length; i++){
+// 	console.log("Name: " + students[i].name + ", Cohort: " + students[i].cohort);
+// }
+
+
+
+// Challenge 2
+// Print out the below object to match the example.
+
+let users = {
+    employees: [
+        {'first_name':  'Miguel', 'last_name' : 'Jones'},
+        {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+        {'first_name' : 'Nora', 'last_name' : 'Lu'},
+        {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+    ],
+    managers: [
+       {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+       {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+    ]
+ };
+
+console.log("EMPLOYEES")
+for (var i = 0; i < users.employees.length; i++){
+	console.log(i + 1 + " - " + 
+		users.employees[i].last_name.toUpperCase() + ", " + 
+		users.employees[i].first_name.toUpperCase() + " - " + 
+		(users.employees[i].first_name.length + users.employees[i].last_name.length));
+}
+console.log("MANAGERS")
+for (var i = 0; i < users.managers.length; i++){
+	console.log(i + 1 + " - " + 
+		users.managers[i].last_name.toUpperCase() + ", " + 
+		users.managers[i].first_name.toUpperCase() + " - " + 
+		(users.managers[i].first_name.length + users.managers[i].last_name.length));
+}
+
